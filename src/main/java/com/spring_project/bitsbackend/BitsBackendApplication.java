42b34890d4a82e2ae1579d8410bbcdc15fa924a3
Rifat-00 <rr.rifat22@gmail.com>
2024-02-22 15:12:46 +0600

    project initialization

---
+++ src/main/java/com/spring_project/bitsbackend/BitsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spring_project.bitsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitsBackendApplication.class, args);
+    }
+
+}
